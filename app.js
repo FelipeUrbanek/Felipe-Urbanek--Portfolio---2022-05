b07ad18ac23c81a0725927b4f7e8f8ba359bc2c6
Felipe Urbanek <urbanekfelipe@gmail.com>
2022-04-28 15:36:39 -0300

    28/04/2022

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+$(function () {
+  emailjs.init('01vCxUMKXY1hiZEq7')
+})
+
 $('.navbar-burger').click(function () {
   var widthbody = $('.body').width()
 
@@ -16,11 +20,62 @@ $('.navbar-burger').click(function () {
 $(document).scroll(function () {
   if ($(document).scrollTop() > 5) {
     $('.navbar').addClass('is-fixed-top')
-    $('.navbar').css({ width: '100%', 'border-radius': '0px 0px 20px 20px' })
+    $('.navbar').css({ width: '100%', 'border-radius': '0px 0px 0px 0px' })
   } else {
     $('.navbar').removeClass('is-fixed-top')
     $('.navbar').css({ 'max-width': '1440px' })
   }
+})
+
+$('#btn-email').click(function () {
+  var templateParams = {
+    name: document.getElementById('input-text').value,
+    email: document.getElementById('input-email').value,
+    mensagem: document.getElementById('input-mensagem-footer').value
+  }
+
+  if (
+    templateParams.name === '' ||
+    templateParams.email === '' ||
+    templateParams.mensagem === ''
+  ) {
+    alert('Preencha todos os campos')
+  } else {
+    emailjs.send('gmail-message', 'template_gydeqlc', templateParams).then(
+      function (response) {
+        console.log('SUCCESS!', response.status, response.text)
+      },
+      function (error) {
+        console.log('FAILED...', error)
+      }
+    )
+    $('#btn-email').css('background', '#282828')
+    $('#btn-email').text('Enviado!')
+
+    document.getElementById('input-text').value = ''
+    document.getElementById('input-email').value = ''
+    document.getElementById('input-mensagem-footer').value = ''
+  }
+})
+
+$('.columns-portfolio img').mouseenter(function () {
+  $(this)
+  $(this).css('-webkit-filter', 'grayscale(0%)')
+  $(this).css('transform', 'scale(1.05)')
+})
+
+$('.columns-portfolio .column').mouseenter(function () {
+  $(this).find('p').css('transform', 'scale(1.05)')
+  $(this).find('button').css('transform', 'scale(1.05)')
+  $(this).find('img').css({ filter: 'grayscale(0%)', transform: 'scale(1.05)' })
+})
+
+$('.columns-portfolio .column').mouseleave(function () {
+  $(this).find('p').css('transform', 'scale(1.00)')
+  $(this).find('button').css('transform', 'scale(1.00)')
+  $(this)
+    .find('img')
+    .css({ filter: 'grayscale(100%)', transform: 'scale(1.00)' })
 })
 
 //is-fixed-top
